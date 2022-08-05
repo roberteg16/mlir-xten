bbfd5f94ca88270c059f2901ab4b95a9c88fb49c
Ferdinand Lemaire <ferdinan@xilinx.com>
2022-08-05 14:54:04 +0200

    Bump llvm

--- lib/Conversion/XTenToLinalg.cpp
+++ lib/Conversion/XTenToLinalg.cpp
@@ -22,7 +22,7 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/Linalg/Transforms/Transforms.h"
-#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Tensor/Utils/Utils.h"
 #include "mlir/Pass/Pass.h"
